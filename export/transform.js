9dfe32f5611a99e81c489a24574cbb85bcad77ae
Tim Hübert <rexrex231@gmail.com>
2024-04-06 17:41:30 +0200

    Update döner locations

--- export/transform.js
+++ export/transform.js
@@ -1,42 +1,33 @@
-function csvToArray(text) {
-    let p = '', row = [''], ret = [row], i = 0, r = 0, s = !0, l;
-    for (l of text) {
-        if ('"' === l) {
-            if (s && l === p) row[i] += l;
-            s = !s;
-        } else if (',' === l && s) l = row[++i] = '';
-        else if ('\n' === l && s) {
-            if ('\r' === p) row[i] = row[i].slice(0, -1);
-            row = ret[++r] = [l = '']; i = 0;
-        } else row[i] += l;
-        p = l;
-    }
-    return ret;
-};
+import fs from 'fs';
 
-const lines = document.documentElement.innerHTML
-    .split("pre-wrap;\">")[1].split("</pre")[0];
+const lines = fs.readFileSync('doener.json', 'utf8');
+
+const data = JSON.parse(lines);
 
 let xml = `<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
 <Document>`;
 
-csvToArray(lines).forEach((parts) => {
-    const name = parts[0];
-    const coords = parts[2].split(',').reverse().join(',').replace(' ', '')
+for (const doener of data) {
+    const name = doener.name;
+    const desc = doener.description;
+    const address = doener.address;
+    let coords = doener.coords.split(',').reverse().join(',').replace(' ', '');
+    const price = doener.price;
+    const rating = doener.rating;
 
     xml += `
     <Placemark>
       <name>${name}</name>
-      <description>${name}</description>
+      <description>${desc}</description>
       <Point>
         <coordinates>${coords}</coordinates>
       </Point>
     </Placemark>
     `
-});
+}
 
 xml += `</Document>
 </kml>`
 
-console.log(xml);
+fs.writeFileSync('../public/data/data.kml', xml);
